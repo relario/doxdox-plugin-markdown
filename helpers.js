5e8f9cf137978ffd077f04e117d9fb0190c678d4
imobiledev <mike@imobiledeveloper.com>
2018-03-23 16:54:34 +0200

    add json stringify helper

--- helpers.js
+++ helpers.js
@@ -1,3 +1,7 @@
 const Handlebars = require('handlebars');
 
 Handlebars.registerHelper('replaceNewLinesWithBR', block => block.replace(/[\r\n]+/g, '<br>'));
+
+Handlebars.registerHelper('json', function(context) {
+    return JSON.stringify(context);
+});
